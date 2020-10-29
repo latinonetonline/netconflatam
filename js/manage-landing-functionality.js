0ce4290375ad19964ece1ec66db3c5b15ad54059
pac09 <pco07@hotmail.com>
2020-10-28 22:06:44 -0600

    Fix para anchor tag en cards y fecha UTC para Cpuntdown es convertida a fecha local del navegador

---
+++ js/manage-landing-functionality.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function() {
+    //Initialize popover component
+    $(function () {
+        $('[data-toggle="popover"]').popover();
+    });
+
+
+    $(".flipper").click(function() {
+        var target = $( event.target );
+
+        if ( target.is("a") ) {
+          //follow that link
+          return true;
+        } else {
+          $(this).toggleClass("flip");
+        }
+        
+        return false;
+      });
+
+    $(".v-flipper").click(function() {		        
+        $(this).toggleClass("v-flip");
+    });
+
+    //Set the date we're counting down to
+    var utcCountDownDate = new Date("11/14/2020 15:00:00 UTC");
+    var localCountDownDate = utcCountDownDate.toLocaleString();
+    const countDownDate = new Date(localCountDownDate).getTime();
+
+    //Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
+        
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        //Outputs the result
+        $(".days").html(days);
+        $(".hours").html(hours);
+        $(".minutes").html(minutes);
+        $(".seconds").html(seconds);
+
+        // If the count down is over, write counter to 0 
+        if (distance < 0) {
+            clearInterval(x);
+                
+            $(".days").html(0);
+            $(".hours").html(0);
+            $(".minutes").html(0);
+            $(".seconds").html(0);
+        }
+    }, 1000);
+
+});
